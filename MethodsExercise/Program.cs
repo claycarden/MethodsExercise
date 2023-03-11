bb48b5e82c755308cf6fa3e0ddf1b3726db2653f
Clay Carden <clayton.a.carden@gmail.com>
2023-03-10 20:15:46 -0600

    Fixed Method Exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -27,19 +27,49 @@
             Console.WriteLine($"{userName} one day dreamed of being a {userDreamJob}.");
             Console.WriteLine($"{userName} secretly wanted to be {userSuperhero}!");
 
-           
-            
-            
-            
+
+
+
+
             Console.WriteLine("Give me a number to add");
             int num1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Give me a number to add to the first one");
-            int num2 = int.Parse(Console.ReadLine()); 
+            int num2 = int.Parse(Console.ReadLine());
 
             int sum = Sum(num1, num2);
             Console.WriteLine($"The sum is: {sum}");
+
+            Console.WriteLine("Give me a number to add");
+            int Num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the first one");
+            int Num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the second one");
+            int num3 = int.Parse(Console.ReadLine());
+
+            int sum2= Sum(Num1, Num2, num3);
+            Console.WriteLine($"The sum2 is: {sum2}");
+
+            Console.WriteLine("Give me a number to add");
+            int NUm1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the first one");
+            int NUm2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the second one");
+            int NUm3 = int.Parse(Console.ReadLine());
             
+            Console.WriteLine("Give me a number to add to the thrid one");
+            int num4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the fourth one");
+            int num5 = int.Parse(Console.ReadLine());
+
+            int sum3 = Sum(NUm1, NUm2, NUm3, num4, num5);
+            Console.WriteLine($"The sum3 is: {sum3}");
+
             Console.WriteLine("Give me a number");
             num1 = int.Parse(Console.ReadLine());
 
